cac0c4bc9be3a4475d1f33f606c28876eaaba98c
malakshaer <shaermk06@gmail.com>
2022-09-30 23:43:16 +0300

    backend: covert to binary api fixed

--- app/Http/Controllers/toBinary.php
+++ app/Http/Controllers/toBinary.php
@@ -6,13 +6,13 @@ use Illuminate\Http\Request;
 
 class ToBinary extends Controller
 {
-    function toBinary($str = "I have 106 sheep"){
+    function toBinary($str){
         
         $int = (int) filter_var($str, FILTER_SANITIZE_NUMBER_INT);
         $res = decbin($int);
         
         return response()->json([
-            "I have $res sheep"
+            $str. $res
         ]);
         
     }
